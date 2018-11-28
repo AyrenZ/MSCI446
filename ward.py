dc97bf42a0a131a1f2c33ff29e0675474d4027c8
RafaelaWu <wyf1085644105@gmail.com>
2018-11-28 17:08:38 +0800

    prediction+updated excel
    jiayou dear

--- ward.py
+++ ward.py
@@ -12,26 +12,25 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 from sklearn import linear_model
+#read the shapes of the wards in toronto
 sf = shapefile.Reader("icitw_wgs84.shp")
 wards = sf.shapeRecords() #list of shapes
-
+#read the 311 data
 restaurantData = pd.read_excel('dinesafedata.xlsx')
 garbage16Data = pd.read_csv("SR2016.csv")
 garbage17Data = pd.read_csv("SR2017.csv")
 garbage18Data = pd.read_csv("SR2018.csv")
 result = list()
-#format of result record:
-#ward name, res_garbage,total garbage, res_waste, total waste, num or 
 severity = restaurantData[restaurantData.SEVERITY.isin(['M - Minor','S - Significant', 'C - Crucial', '4'])]
 x=list()
-y_ward,y_s,y_m,y_c=list(),list(),list(),list()
 y=list()    
+ward_waste=dict()
 for i in range(len(wards)):
     name = wards[i].record[2]
     if name.index(')')-name.index('(')==2:     #fix the naming issue for (6) and (06)
         name=name[0:name.index('(')]+'(0'+name[name.index('(')+1:]
+    # define point in shape
     points = wards[i].shape.points
-
     bbPath = mplPath.Path(points)
     #collect severity info for this ward
     sev_ward = [a for a in severity.values if bbPath.contains_point((a[7],a[6]))]
@@ -39,11 +38,9 @@ for i in range(len(wards)):
     sev_m = [a for a in sev_ward if a[12]=="M - Minor"]
     sev_s = [a for a in sev_ward if a[12]=="S - Significant"]
     sev_c = [a for a in sev_ward if a[12]=="C - Crucial"]
-#    record= [name,len(res_garbage),len(garbage),len(res_waste),len(waste),len(sev_ward),len(sev_m),len(sev_s),len(sev_c)]
-#    result.append(record)
-    for yr in range(1,3):
-        #collect number of garbage request in this ward
-
+    temp_waste=dict()
+    for yr in range(0,3):
+        #collect number of garbage request in this ward each year
         if yr ==0:
             all_ward = [rec for rec in garbage16Data.values if rec[5]==name and rec[1]!="Cancelled"]
             time='2016-'
@@ -58,7 +55,9 @@ for i in range(len(wards)):
         waste = [a for a in all_ward if 'Waste' in a[6]]
         res_waste = [a for a in waste if 'Residential' in a[6]]
         monthlist =['01','02','03','04','05','06','07','08','09','10','11','12']
+        # the dine safe dataset ends at 2018-09
         if yr==2: monthlist = ['01','02','03','04','05','06','07','08','09']
+        
         for m in monthlist:
             
             month = time+m
@@ -67,36 +66,23 @@ for i in range(len(wards)):
             w=[a for a in waste if a[0][:7]==month]
             rw=[a for a in res_waste if a[0][:7]==month]
             war = [a for a in sev_ward if a[11][:7]==month]
-#            if len(war)==0:
-#                y.append(0)
-#            else:
-#                y.append(1)
-#            x_record = [len(g),len(g)-len(rg),len(w),len(w)-len(rw)]
-#            x.append(x_record)
-            s = [a for a in sev_s if a[11][:7]==month]
-            v = [a for a in sev_m if a[11][:7]==month]
-            c = [a for a in sev_c if a[11][:7]==month]
+            if len(war)==0:
+                y.append(0)
+            else:
+                y.append(1)
             x_record = [len(g),len(g)-len(rg),len(w),len(w)-len(rw)]
             x.append(x_record)
-            y_ward.append(len(war))#,len(sev_m),len(sev_s),len(sev_c)]
-            y_m.append(len(v))
-            y_s.append(len(s))
-            y_c.append(len(c))
-            print(month,name,len(g),len(g)-len(rg),len(w),len(w)-len(rw),len(war),len(v),len(s),len(c))
-            record = [name,month,len(g),len(g)-len(rg),len(w),len(w)-len(rw),len(war),len(v),len(s),len(c)]
-            result.append(record)
-#model = linear_model.LinearRegression().fit(np.array(x),np.array(y))
-#print(model.score(x, y))
-#regr = RandomForestRegressor(max_depth=4, random_state=0,n_estimators=10).fit(x[:400],y_ward[:400])
-#print("total sev:",regr.score(x[400:],y_ward[400:]))
-#regr = RandomForestRegressor(max_depth=4, random_state=0,n_estimators=100).fit(x,y_m)
-#print("M - Minor",regr.score(x,y_m))
-#regr = RandomForestRegressor(max_depth=4, random_state=0,n_estimators=10).fit(x,y_c)
-#print("C - Crucial",regr.score(x,y_c))
-#regr = RandomForestRegressor(max_depth=4, random_state=0,n_estimators=10).fit(x,y_s)
-#print("S - Significant",regr.score(x,y_s))
-#from sklearn.neighbors import KNeighborsClassifier
-#neigh = KNeighborsClassifier(n_neighbors=3)
-#neigh.fit(x, y) 
-#print(neigh.score(x,y))
-    
+            temp_waste[month]=[len(g)-len(rg)+len(w)-len(rw),len(rg)+len(rw)]
+    ward_waste[name]=temp_waste
+            
+
+#predict the food related issue with garbage data
+from sklearn.neighbors import KNeighborsClassifier
+neigh = KNeighborsClassifier(n_neighbors=4)
+#fit on the first 1000 data points, predict on the last 400 to validate
+neigh.fit(x[:1000], y[:1000]) 
+print(neigh.score(x[1000:],y[1000:]))            
+
+#print out ward_waste value for excel generation
+print(ward_waste)
+
